9b6b1e1b42ea5f68345fd7d9eb33c69d79047e1d
Zach Del <zach.delapenha.developer@gmail.com>
2024-02-28 14:38:12 +0000

    4th commit

--- client/src/components/OpeningHours.tsx
+++ client/src/components/OpeningHours.tsx
@@ -1,4 +1,3 @@
-
 import pub from "../assets/pubfront.jpg";
 
 const OpeningHours = () => {
@@ -7,17 +6,19 @@ const OpeningHours = () => {
       className="relative "
       style={{
         backgroundImage: `url(${pub})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
       }}
     >
       <div className="bg-white bg-opacity-100 p-6">
-        <h3 className="font-bold  text-center md:text-lg  mb-6">- OPENING HOURS -</h3>
+        <h3 className="font-bold  text-center md:text-lg  mb-6">
+          - OPENING HOURS -
+        </h3>
         <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-1 gap-4 text-center">
           {/* Hours for Monday - Friday */}
           <div>
-            <p className="font-bold text-xl">12 AM - 11 PM All Week!</p>
+            <p className="font-bold text-xl">12 AM - 12 PM All Week!</p>
           </div>
         </div>
       </div>
