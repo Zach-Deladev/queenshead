e96b8f72e88c0c188caa7b206f999586d25307ee
Zach Del <zach.delapenha.developer@gmail.com>
2024-03-01 22:19:16 +0000

    Added pdf menu.

--- client/src/components/MenuSection.tsx
+++ client/src/components/MenuSection.tsx
@@ -1,18 +1,13 @@
-import { useState } from "react";
 import drinksImage from "../assets/drinks.png";
 import Gallery from "./Gallery";
 import image1 from "../assets/pint.jpg";
 import image2 from "../assets/fishnchips.jpg";
 import image3 from "../assets/pintoutside.jpg";
 import image4 from "../assets/pienchips.jpg";
-import menu from "../assets/menu.webp";
+import menuPdf from "../assets/menupdf.pdf"; // Import your PDF file here
 
 const MenuSection = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  const toggleModal = () => {
-    setShowModal(!showModal);
-  };
+  // Removed useState hook for showModal
 
   const images = [
     { src: image1, alt: "pint" },
@@ -40,31 +35,14 @@ const MenuSection = () => {
           Food & Drinks
         </h1>
         <Gallery images={images} />
-        <button
-          type="button"
-          onClick={toggleModal}
+        <a
+          href={menuPdf} // Link to your PDF file
+          download="Menu.pdf" // Optional: Specify a filename for the downloaded file
           className="py-1 px-3 mt-3 mb-4 bg-transparent border-4 border-[#BB945C] text-[#FFFAE2] font-bold focus:outline-none focus:shadow-outline hover:bg-[#BB945C]"
         >
-          View Menu
-        </button>
+          Download Menu
+        </a>
       </div>
-      {showModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div className="bg-black p-4 border-[#BB945C] border-2 shadow-lg flex flex-col items-center">
-            <img
-              src={menu}
-              alt="Menu"
-              className="border-[#BB945C] border-2 mb-4 max-w-xs md:max-w-xl"
-            />
-            <button
-              onClick={toggleModal}
-              className="mt-4 py-1 px-3 bg-[#BB945C] text-white font-bold rounded"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
